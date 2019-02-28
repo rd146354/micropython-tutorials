3efdf75a0a4da5bdcd04d258609eb15e0ddb243a
rd146354 <rdurrett@msudenver.edu>
2019-02-27 19:15:08 -0700

    Added some files.

---
+++ buttons-conditional.py
@@ -0,0 +1,24 @@
+# Admittedly this is pretty heavily modified from the example
+# provided in the tutorial
+
+from microbit import *
+
+while True:
+    sleep(100)
+    (a, b) = (button_a.was_pressed(), button_b.was_pressed())
+    if a and b:
+        display.show(Image.SURPRISED)
+        sleep(1000)
+    elif a:
+        display.show(Image.HAPPY)
+        sleep(1000)
+    elif b:
+        display.show(Image.ARROW_W)
+        sleep(1000)
+    elif pin0.is_touched():
+        break
+    else:
+        display.show(Image.SAD)
+
+display.clear()
+
